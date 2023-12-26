662e8a8a66383b82c8cc5def545f413a0bb9256e
Asaf Nahmias <asafn2004@gmail.com>
2023-12-26 15:06:39 +0200

    deploy

--- backend/server.ts
+++ backend/server.ts
@@ -3,13 +3,10 @@ import config from "./Utils/Config";
 import cors from "cors";
 import ErrorHandler from "./Middleware/route-not-found";
 import sendEmailRouter from "./Routes/sendEmailRoute";
-import path from 'path';
+import path from "path";
 // create server
 
 const server = express();
-
-
-
 
 //cors = cross origin resource sharing...
 server.use(cors());
@@ -18,26 +15,25 @@ server.use(cors());
 server.use(express.json());
 
 //static images and videos from the backend
-server.use('/images',express.static("images"));
-server.use('/video',express.static("video"));
+server.use("/images", express.static("images"));
+server.use("/video", express.static("video"));
 
 //use the build files
-const _dirname = path.dirname("")
-const buildPath = path.join(_dirname , "../frontend/build");
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../frontend/build");
 
-server.use(express.static(buildPath))
+server.use(express.static(buildPath));
 
-server.get("/*", function(req, res){
-
-      res.sendFile(
-        path.join(__dirname, "../frontend/build/index.html"),
-        function (err) {
-          if (err) {
-            res.status(500).send(err);
-          }
-        }
-      );
-})
+server.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "../frontend/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 
 //using routes => localhost:5000/api/v1
 server.use("/api/", sendEmailRouter);
