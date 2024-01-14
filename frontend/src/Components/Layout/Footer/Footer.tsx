50620ddd20604f5b88ae2de53c82156081f455b6
Asaf Nahmias <asafn2004@gmail.com>
2024-01-14 17:02:50 +0200

    update

--- frontend/src/Components/Layout/Footer/Footer.tsx
+++ frontend/src/Components/Layout/Footer/Footer.tsx
@@ -20,7 +20,7 @@ function Footer(): JSX.Element {
     e.preventDefault();
     // Send form data to asafn2004@gmail.com
     try {
-      const response = await axios.post("http://localhost:5000/api/sendEmail", {
+      const response = await axios.post("http://3.70.229.210/api/sendEmail", {
         name,
         pNumber,
         email,
