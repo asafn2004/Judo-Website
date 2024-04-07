67e14acab64f1b6230518905e06227acbd3a7009
Asaf Nahmias <asafn2004@gmail.com>
2024-04-07 04:17:09 +0300

    mm

--- frontend/src/Components/Layout/Footer/Footer.tsx
+++ frontend/src/Components/Layout/Footer/Footer.tsx
@@ -21,7 +21,7 @@ function Footer(): JSX.Element {
     e.preventDefault();
     // Send form data to asafn2004@gmail.com
     try {
-      const response = await axios.post("http://judocenters.com/api/sendEmail", {
+      const response = await axios.post("https://judocenters.com/api/sendEmail", {
         name,
         pNumber,
         email,
